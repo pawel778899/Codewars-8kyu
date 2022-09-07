7b4972efacfa70e43167bfc89ab1c74696f9eba3
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 12:31:41 +0200

    Create ReturningStrings.java

---
+++ ReturningStrings.java
@@ -0,0 +1,16 @@
+public class Kata
+{
+  public static String greet(String name)
+  {  
+    return "Hello, "+name+" how are you doing today?";  
+  }
+}
+
+
+
+// DESCRIPTION:
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// STRINGS FUNDAMENTALS
