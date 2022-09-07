813c30e4c0ea2a01e147f913e3cd185c02f742f6
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 13:11:33 +0200

    Create VolumeOfACuboid.java

---
+++ VolumeOfACuboid.java
@@ -0,0 +1,11 @@
+public class Kata {
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    return length*width*height;
+  }
+}
+
+
+// DESCRIPTION:
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+// GEOMETRY FUNDAMENTALS
