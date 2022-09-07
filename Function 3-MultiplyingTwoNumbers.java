fe6a1b6617b46a2b6c9bae7c0c323a25f6c0849a
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 12:40:53 +0200

    Create Function 3-MultiplyingTwoNumbers.java

---
+++ Function 3-MultiplyingTwoNumbers.java
@@ -0,0 +1,14 @@
+public class Kata {
+  public int multiply(int a, int b){
+    return a*b;    
+  }
+}
+
+
+DESCRIPTION:
+Implement a function called multiply, which takes two numbers and returns their product:
+
+multiply(2, 3) = 6
+multiply(0, 188) = 0
+multiply(85, 144) = 12240
+FUNDAMENTALS
