e108c45732c221d98b4e1bb32578b0f8b309a82f
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 08:22:48 +0200

    Update Jenny'sSecretMessage.java

--- Jenny'sSecretMessage.java
+++ Jenny'sSecretMessage.java
@@ -1,7 +1,5 @@
 public class Greeter {
-  public static String greet(String name) {
-    
-        
+  public static String greet(String name) {           
     if(name.equals("Johnny")){
       return "Hello, my love!";
     }
