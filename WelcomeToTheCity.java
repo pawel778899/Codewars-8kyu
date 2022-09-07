c9de4c68ddaa2eeb560b609e2036db8ade7bde06
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:05:13 +0200

    Create WelcomeToTheCity.java

---
+++ WelcomeToTheCity.java
@@ -0,0 +1,20 @@
+public class Hello{
+  public String sayHello(String [] name, String city, String state){
+       return String.format("Hello, %s! Welcome to %s, %s!", String.join(" ", name), city, state);
+  }
+}
+
+
+
+
+
+
+// DESCRIPTION:
+// Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+// Example:
+
+// sayHello(new String[]{"John", "Smith"}, "Phoenix", "Arizona")
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+// STRINGS FUNDAMENTALS
