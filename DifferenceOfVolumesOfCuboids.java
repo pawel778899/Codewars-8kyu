405b14a0d3fbc27b21b4dbc87d6ead7a35612d0e
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 13:31:02 +0200

    Update DifferenceOfVolumesOfCuboids.java

--- DifferenceOfVolumesOfCuboids.java
+++ DifferenceOfVolumesOfCuboids.java
@@ -6,15 +6,15 @@ public class CuboidVolumes {
 
 
 
-DESCRIPTION:
-In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+// DESCRIPTION:
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
 
-For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
 
-Your function will be tested with pre-made examples as well as random ones.
+// Your function will be tested with pre-made examples as well as random ones.
 
-If you can, try writing it in one line of code.
+// If you can, try writing it in one line of code.
 
-FUNDAMENTALS
+// FUNDAMENTALS
 
 
