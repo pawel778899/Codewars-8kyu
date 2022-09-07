d1832759f402be8510d0b36de0ed1b66ff957bf5
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:42:55 +0200

    Create HexToDecimal.java

---
+++ HexToDecimal.java
@@ -0,0 +1,13 @@
+public class Kata {
+  public static int hexToDec(final String hexString) {
+       return Integer.parseInt(hexString, 16);
+  }
+ }
+
+
+
+
+// DESCRIPTION:
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// FUNDAMENTALS
