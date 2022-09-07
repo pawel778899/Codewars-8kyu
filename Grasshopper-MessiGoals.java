90b1a0a13cc737991db0593108124dfdb6027045
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:49:55 +0200

    Create Grasshopper-MessiGoals.java

---
+++ Grasshopper-MessiGoals.java
@@ -0,0 +1,29 @@
+class Goals {
+  public static int laLigaGoals=43;
+  public static int championsLeagueGoals=10;
+  public static int copaDelReyGoals=5;
+  
+  public static int totalGoals=laLigaGoals+championsLeagueGoals+copaDelReyGoals;
+}
+
+
+
+
+// DESCRIPTION:
+// Messi's Goal Total
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+// FUNDAMENTALS
