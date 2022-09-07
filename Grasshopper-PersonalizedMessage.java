848fa257418d8af256ecae6257f592bfdc3c2ee6
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:47:43 +0200

    Create Grasshopper-PersonalizedMessage.java

---
+++ Grasshopper-PersonalizedMessage.java
@@ -0,0 +1,21 @@
+class Kata {
+    static String greet(String name, String owner) {
+        if (name.equals(owner)) {
+        return "Hello boss";
+      }else {
+       return "Hello guest"; 
+      }
+ }
+}
+
+
+
+// DESCRIPTION:
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+// FUNDAMENTALS
