77c0d0f1256e0049b9efed594b9520b28d1e92ff
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 12:34:33 +0200

    Create MakeUpperCase.java

---
+++ MakeUpperCase.java
@@ -0,0 +1,11 @@
+class Upper{
+  public static String MakeUpperCase(String str){
+    System.out.println(str.toUpperCase());
+  return str.toUpperCase();
+  }
+}
+
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+// FUNDAMENTALS
