dff83603576f9d47d91562ad1de1417dd21ba836
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 11:19:46 +0200

    Create Grasshopper-CheckForFactor.java

---
+++ Grasshopper-CheckForFactor.java
@@ -0,0 +1,31 @@
+public class Kata {
+    public static boolean checkForFactor(int base, int factor) {
+        
+      if (base%factor==0){
+    return true;
+  }
+  else {
+    return false;
+   }
+  }
+ }
+
+
+
+// DESCRIPTION:
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+// About factors
+// Factors are numbers you can multiply together to get another number.
+
+// 2 and 3 are factors of 6 because: 2 * 3 = 6
+
+// You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.
+// You can use the mod operator (%) in most languages to check for a remainder
+// For example 2 is not a factor of 7 because: 7 % 2 = 1
+
+// Note: base is a non-negative number, factor is a positive number.
+
+// MATHEMATICS FUNDAMENTALS
