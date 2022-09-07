d4e7f808b815c3bbc53451558d4ece9960d53159
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:12:50 +0200

    Create TransportationOnVacation.java

---
+++ TransportationOnVacation.java
@@ -0,0 +1,23 @@
+public class Kata {
+  private static final int COST_PER_DAY = 40;
+  
+  public static int rentalCarCost(int d) {
+    if (d < 3)       return d * COST_PER_DAY;
+    else if (d >= 7) return d * COST_PER_DAY - 50;
+    else             return d * COST_PER_DAY - 20;
+  }
+}
+
+
+
+
+// DESCRIPTION:
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+// Write a code that gives out the total amount for different days(d).
+
+// FUNDAMENTALS
