f65a82ae7e45575c0451531a5d382aae9dd4ff23
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:01:14 +0200

    Create Grasshopper-GradeBook.java

---
+++ Grasshopper-GradeBook.java
@@ -0,0 +1,27 @@
+public class GrassHopper {
+
+    public static char getGrade(int s1, int s2, int s3) {
+
+      int average = Math.round(s1 + s2 + s3) / 3;
+        return average >= 90 ? 'A' : average >= 80 ? 'B' : average >= 70 ? 'C' : average >= 60 ? 'D' : 'F';
+    }
+}
+
+
+
+
+
+
+// DESCRIPTION:
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+// FUNDAMENTALS
