1df04715957748f06491561fbd3d14478a5144bd
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:01:56 +0200

    Create KataExampleTwist.java

---
+++ KataExampleTwist.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class KataExampleTwist
+{
+    public static String[] kataExampleTwist()
+    {
+        String[] websites = new String[1000];
+        Arrays.fill(websites,"codewars");
+        return websites;
+    }
+}
+
+
+
+
+// DESCRIPTION:
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+// FUNDAMENTALS
