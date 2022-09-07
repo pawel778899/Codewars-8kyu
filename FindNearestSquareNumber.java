5af11b80afbb258372a8179ee30f55190510ef07
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 08:36:41 +0200

    Create FindNearestSquareNumber.java

---
+++ FindNearestSquareNumber.java
@@ -0,0 +1,26 @@
+public class CodeWarsMath {
+  public static int nearestSq(final int n){
+    return (int) Math.pow(Math.round(Math.sqrt(n)), 2);
+  }
+}
+
+
+
+
+
+
+
+// DESCRIPTION:
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// Goodluck :)
+
+// Check my other katas:
+
+// Alphabetically ordered
+
+// Case-sensitive!
+
+// Not prime numbers
+
+// FUNDAMENTALS
