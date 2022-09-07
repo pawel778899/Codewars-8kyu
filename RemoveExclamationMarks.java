28e4b805b384b6102e22507ed3cacbfa8bda1581
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:53:02 +0200

    Create RemoveExclamationMarks.java

---
+++ RemoveExclamationMarks.java
@@ -0,0 +1,13 @@
+class Solution {
+    static String removeExclamationMarks(String s) {
+      return s.replaceAll("!","");
+      
+    }
+}
+
+
+
+// DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// FUNDAMENTALS
