19b58b51dd2384782205a26c137d56abb3da4098
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 11:01:55 +0200

    Create ConvertABooleanToAString.java

---
+++ ConvertABooleanToAString.java
@@ -0,0 +1,13 @@
+public class BooleanToString {
+  public static String convert(boolean b){
+    return b ? "true" : "false";
+  }
+}
+
+
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// STRINGS FUNDAMENTALS
