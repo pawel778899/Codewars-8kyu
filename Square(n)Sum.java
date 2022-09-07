3fb72b0f3afae743883d76051da93455934f1669
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 08:15:27 +0200

    Create Square(n)Sum.java

---
+++ Square(n)Sum.java
@@ -0,0 +1,15 @@
+public class Sleigh {
+  public static Boolean authenticate(String name, String password){
+     return name.equals("Santa Claus") && password.equals("Ho Ho Ho!");
+   }
+
+
+
+
+
+// DESCRIPTION:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+// ARRAYSLISTSFUNDAMENTALS}
