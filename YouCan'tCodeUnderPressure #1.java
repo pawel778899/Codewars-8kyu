83af76ba5d0ec4fed860ae51cb4f630e16a56586
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:41:21 +0200

    Create YouCan'tCodeUnderPressure #1.java

---
+++ YouCan'tCodeUnderPressure #1.java
@@ -0,0 +1,11 @@
+class Java {
+  public static int doubleInteger(int i) {
+    return i+i;
+  }
+}
+
+
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+// FUNDAMENTALS
