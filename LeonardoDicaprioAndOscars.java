f4e47e40e0c9730bb58684960c71d219e468796f
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 08:51:09 +0200

    Create LeonardoDicaprioAndOscars.java

---
+++ LeonardoDicaprioAndOscars.java
@@ -0,0 +1,23 @@
+public class Kata
+{
+  public static String leo(final int oscar){
+    return oscar == 88 ? "Leo finally won the oscar! Leo is happy" : oscar == 86 ? "Not even for Wolf of wallstreet?!"
+                : oscar > 88 ? "Leo got one already!" : "When will you give Leo an Oscar?";
+  }
+}
+
+
+
+
+
+// DESCRIPTION:
+// You have to write a function that describe Leo:
+
+// def leo(oscar):
+//   pass
+// if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+// if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+// if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+// if it was over 88 you should return "Leo got one already!"
+
+// FUNDAMENTALS
