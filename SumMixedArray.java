ed0e9546718e764cb9b7a48c93ec17b9e3b59657
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:49:17 +0200

    Create SumMixedArray.java

---
+++ SumMixedArray.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class MixedSum {
+
+  public int sum(List<?> mixed) { 
+     return mixed.stream().mapToInt(o -> Integer.parseInt(o.toString())).sum();
+  }
+}
+
+
+
+DESCRIPTION:
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+FUNDAMENTALS STRINGS ARRAYS
+
