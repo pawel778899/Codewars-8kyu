e87eb498b84687568a31bcca11baf1a14887c1f3
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 08:22:30 +0200

    Create Jenny'sSecretMessage.java

---
+++ Jenny'sSecretMessage.java
@@ -0,0 +1,23 @@
+public class Greeter {
+  public static String greet(String name) {
+    
+        
+    if(name.equals("Johnny")){
+      return "Hello, my love!";
+    }
+    else{
+      return String.format("Hello, %s!", name);
+    }
+  }
+}
+
+
+
+
+
+// DESCRIPTION:
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+// Can you help her?
+
+// DEBUGGING
