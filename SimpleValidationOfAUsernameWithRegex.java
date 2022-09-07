1855e7afc1a8c869b6794edcdf9b43d1949039fc
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 08:38:47 +0200

    Create SimpleValidationOfAUsernameWithRegex.java

---
+++ SimpleValidationOfAUsernameWithRegex.java
@@ -0,0 +1,21 @@
+public class ZywOo {
+  public static boolean validateUsr(String s) {
+  return s.matches("[a-z_\\d]{4,16}");
+  }
+}
+
+
+
+
+
+
+
+// DESCRIPTION:
+// Write a simple regex to validate a username. Allowed characters are:
+
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+
+// REGULAR EXPRESSIONS FUNDAMENTALS
