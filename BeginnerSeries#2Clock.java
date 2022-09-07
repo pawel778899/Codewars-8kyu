32fce16473886747a98f979b81809adc804cab92
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 12:13:50 +0200

    Create BeginnerSeries#2Clock.java

---
+++ BeginnerSeries#2Clock.java
@@ -0,0 +1,27 @@
+public class Clock
+{
+  public static int Past(int h, int m, int s) 
+  {
+    return ((h*60*60)+(m*60)+s)*1000;     
+  }
+}
+
+
+
+// DESCRIPTION:
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+// FUNDAMENTALS
