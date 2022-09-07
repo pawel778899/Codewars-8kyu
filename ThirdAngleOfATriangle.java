0dd1fa292e6c0527d69b76aa4c7238f11b73e126
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 13:07:25 +0200

    Create ThirdAngleOfATriangle.java

---
+++ ThirdAngleOfATriangle.java
@@ -0,0 +1,22 @@
+public class ThirdAngle {
+    public static int otherAngle(int angle1, int angle2) {     
+      if (angle1 >0 && angle2 >0){
+        return 180-angle1-angle2;
+      }
+      else {
+          return 0;
+      }     
+    }
+}
+
+
+// DESCRIPTION:
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+// FUNDAMENTALS
