225314dd148d97d2fda7081ec96d494e09e48cbe
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:08:30 +0200

    Create GetCharacterFromASCIIValue.java

---
+++ GetCharacterFromASCIIValue.java
@@ -0,0 +1,21 @@
+public class Ascii {
+  public static char getChar(int c) {
+     return (char) c; 
+  }
+}
+
+
+
+
+// DESCRIPTION:
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// get_char(65)
+// should return:
+
+// 'A'
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+// FUNDAMENTALS
