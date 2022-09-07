19b352c2d4774475077b506ca3a60290206bf0f7
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 08:12:13 +0200

    Create BinToDecimal.java

---
+++ BinToDecimal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Converter{
+  public static int binToDecimal(String inp){
+       return Integer.parseInt(inp,2);
+  }
+}
+
+
+
+
+DESCRIPTION:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+BINARY FUNDAMENTALS
