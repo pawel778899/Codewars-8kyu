f06ed0a9c33657fe154472f99857df03883d2c96
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 12:43:43 +0200

    Create QuarterOfTheYear.java

---
+++ QuarterOfTheYear.java
@@ -0,0 +1,30 @@
+public class Kata {
+    public static int quarterOf(int month) {
+      
+                if (month==1||month==2||month==3) {
+                return 1;
+                }
+                else if (month==4||month==5||month==6) {
+                 return 2;
+                  }
+                else if (month==7||month==8||month==9) {
+                 return 3;
+                }
+                 else if (month==10||month==11||month==12) {
+                 return 4;
+                 }
+                else {
+                return 0;
+                }
+    }
+}
+
+
+
+
+// DESCRIPTION:
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+// For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+// FUNDAMENTALS
