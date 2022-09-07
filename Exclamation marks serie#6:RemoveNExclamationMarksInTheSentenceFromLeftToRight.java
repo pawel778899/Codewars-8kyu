b0e4ba21f19bb1ff2276341ad7e010c5c0c791ae
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 08:32:54 +0200

    Create Exclamation marks serie#6:RemoveNExclamationMarksInTheSentenceFromLeftToRight.java

---
+++ Exclamation marks serie#6:RemoveNExclamationMarksInTheSentenceFromLeftToRight.java
@@ -0,0 +1,29 @@
+public class Kata {
+      public static String remove(String s, int n) {
+        while (n-- > 0) s = s.replaceFirst("!", "");
+        return s;
+   }
+}
+  
+
+
+
+
+
+// DESCRIPTION:
+// Description:
+// Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+// Examples
+// remove("Hi!",1) === "Hi"
+// remove("Hi!",100) === "Hi"
+// remove("Hi!!!",1) === "Hi!!"
+// remove("Hi!!!",100) === "Hi"
+// remove("!Hi",1) === "Hi"
+// remove("!Hi!",1) === "Hi!"
+// remove("!Hi!",100) === "Hi"
+// remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+// remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+// FUNDAMENTALS
