cce89547f6cebf008341cc3f7f9cf7cca8cfc92c
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 09:42:50 +0200

    Update Grasshopper-ArrayMean.java

--- Grasshopper-ArrayMean.java
+++ Grasshopper-ArrayMean.java
@@ -1,3 +1,4 @@
+//Soultion 1 (for)
 public class GrassHopper {
     
     public static int findAverage(int[] nums) {
@@ -9,6 +10,19 @@ public class GrassHopper {
     }
 }
 
+//Solution 2 (for each)
+
+public class GrassHopper {
+    
+    public static int findAverage(int[] nums) {
+
+      int sum=0;
+      for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return (sum/nums.length);
+    }
+}
 
 
 
