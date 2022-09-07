4af0259c069659cb7d3b5f03fb7484f9cde14eea
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:09:46 +0200

    Create IsItEven?.java

---
+++ IsItEven?.java
@@ -0,0 +1,27 @@
+public class Number {
+
+  public boolean isEven(double n) {
+     
+    if(n%2==0)
+      {
+      return true;
+    }
+    return false;     
+  }
+}
+
+
+
+
+// DESCRIPTION:
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+// FUNDAMENTALS
