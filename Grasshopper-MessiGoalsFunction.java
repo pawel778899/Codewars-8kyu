bce5a3b7f7db28448c632787fe04390214cd46e0
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 12:54:30 +0200

    Create Grasshopper-MessiGoalsFunction.java

---
+++ Grasshopper-MessiGoalsFunction.java
@@ -0,0 +1,23 @@
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+      return (laLigaGoals + copaDelReyGoals + championsLeagueGoals);
+    }
+}
+
+
+
+// DESCRIPTION:
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+// FUNDAMENTALS
