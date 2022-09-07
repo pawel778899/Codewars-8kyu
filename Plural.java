3dfd399ebbff629db74c877940497dc4db298b24
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:53:43 +0200

    Create Plural.java

---
+++ Plural.java
@@ -0,0 +1,16 @@
+public class Plural{
+  public static boolean isPlural(float f){
+    return f==1 ? false : true;
+    }
+  }
+
+
+
+// DESCRIPTION:
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+// All values will be positive integers or floats, or zero.
+
+// FUNDAMENTALS
