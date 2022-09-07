11de1ef8c70d8ccdc0e9e20d4db6c1e3dcc2df94
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 10:27:50 +0200

    Create Reversed Strings.java

---
+++ Reversed Strings.java
@@ -0,0 +1,14 @@
+public class Kata {
+  public static String solution(String str) {
+    return new StringBuilder(str).reverse().toString();
+  }
+}
+
+
+
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// STRINGSFUNDAMENTALS
