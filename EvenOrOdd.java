238a9882bf1112612a5ea40a8a3272693d7701d6
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-07 12:25:49 +0200

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,18 @@
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        
+      if (number%2==0){
+        return "Even"; 
+     }
+      else {
+        return "Odd";
+      }     
+    }
+}
+
+
+
+// DESCRIPTION:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// MATHEMATICS FUNDAMENTALS
