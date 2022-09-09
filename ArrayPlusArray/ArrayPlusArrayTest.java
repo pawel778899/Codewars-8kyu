8e05df35afeea775cff65ba731ea7a1a118b89d1
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-09 10:06:05 +0200

    Create ArrayPlusArrayTest.java

---
+++ ArrayPlusArray/ArrayPlusArrayTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class SolutionTest {
+    @Test
+    public void sampleTests() {
+        assertEquals(21, ArrayPlusArray.arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
+        assertEquals(-21, ArrayPlusArray.arrayPlusArray(new int[]{-1,-2,-3}, new int[]{-4,-5,-6}));
+        assertEquals(15, ArrayPlusArray.arrayPlusArray(new int[]{0,0,0}, new int[]{4,5,6}));
+        assertEquals(2100, ArrayPlusArray.arrayPlusArray(new int[]{100,200,300}, new int[]{400,500,600}));
+        assertEquals(100,ArrayPlusArray.arrayPlusArray(new int[]{10,20,30}, new int[] {40,0,0}));
+
+    }
+}
