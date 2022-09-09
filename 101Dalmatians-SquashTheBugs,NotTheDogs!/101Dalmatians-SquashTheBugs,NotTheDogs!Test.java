528dcd2deeaee3249eeb716166f9345d20cfdc3d
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-09 11:41:30 +0200

    Create  101Dalmatians-SquashTheBugs,NotTheDogs!Test.java

---
+++ 101Dalmatians-SquashTheBugs,NotTheDogs!/101Dalmatians-SquashTheBugs,NotTheDogs!Test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+
+    @Test
+    public void basicTests() {
+        assertEquals("More than a handful!",ZywOo.howManyDalmatians(26));
+        assertEquals("Hardly any" ,ZywOo.howManyDalmatians(8));
+        assertEquals("More than a handful!" ,ZywOo.howManyDalmatians(14));
+        assertEquals("Woah that's a lot of dogs!" ,ZywOo.howManyDalmatians(80));
+        assertEquals("Woah that's a lot of dogs!" ,ZywOo.howManyDalmatians(100));
+        assertEquals("101 DALMATIANS!!!" ,ZywOo.howManyDalmatians(101));
+    }
+}
