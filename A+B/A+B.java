7103e6be7e20a0b6abbf3f6124a49f75a1371bf3
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-09 10:21:03 +0200

    Create A+B.java

---
+++ A+B/A+B.java
@@ -0,0 +1,15 @@
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+    
+        return (byte)(a+b);
+    }
+}
+
+
+
+
+
+// DESCRIPTION:
+// Vasya Pupkin just started learning Java and C#. At first, he decided to write simple program that was supposed to sum up two small numbers (numbers and their sum fit in a byte), but it didn't work. Vasya was too sad to find out what is wrong. Help him to correct the mistake.
+
+// FUNDAMENTALS DEBUGGING
