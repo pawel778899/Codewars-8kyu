f6582643d89b6741a50ec36f7429a97d4b024d33
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-09 10:32:46 +0200

    Create AreaOrPerimeter.java

---
+++ AreaOrPerimeter/AreaOrPerimeter.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public static int areaOrPerimeter (int l, int w) {    
+      if(l==w){
+        return l*w;
+      }
+      else {
+        return (2*l+2*w);
+      }  
+    }
+}
+
+
+
+
+// DESCRIPTION:
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// Example(Input1, Input2 --> Output):
+
+// 6, 10 --> 32
+// 3, 3 --> 9
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+// FUNDAMENTALS
