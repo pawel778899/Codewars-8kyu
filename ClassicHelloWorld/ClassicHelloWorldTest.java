a1fa2b40585d8375d09f736139abbb69b232794b
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-10 16:00:35 +0200

    Create ClassicHelloWorldTest.java

---
+++ ClassicHelloWorld/ClassicHelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// TODO: Replace examples and use TDD by writing your own tests
+
+class SolutionTest {
+    @Test
+    void testSomething() {
+        // assertEquals("expected", "actual");
+    }
+}
