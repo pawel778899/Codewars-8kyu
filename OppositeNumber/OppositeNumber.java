4750d77463fc07af79f8a62ddea4ca680f57b367
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-12 12:11:06 +0200

    Create OppositeNumber.java

---
+++ OppositeNumber/OppositeNumber.java
@@ -0,0 +1,19 @@
+public class Kata
+    {
+        public static int opposite(int number)
+        {         
+          return -number;
+        }
+  }
+
+
+
+// DESCRIPTION:
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+// FUNDAMENTALS
