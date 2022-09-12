1a47ed2d86be4aa1c71f780a071e4fcbfbafc961
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-12 10:40:17 +0200

    Create  BeginnerSeries#2ClockTest.java

---
+++ BeginnerSeries#2Clock/BeginnerSeries#2ClockTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class ClockTest {
+    @Test    
+    public void test1(){
+
+    assertEquals(61000, Clock.Past(0,1,1));
+    }
+
+  
+}
