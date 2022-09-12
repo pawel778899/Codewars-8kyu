56a9fb9b52ae5d8fceedc4309749d816ef0ddb35
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-12 10:29:56 +0200

    Create  BasicMathematicalOperationsTest.java

---
+++ BasicMathematicalOperations/BasicMathematicalOperationsTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest
+{
+  @Test
+  public void testBasics()
+  {
+    System.out.println("Basic Tests");
+    assertThat(BasicOperations.basicMath("+", 4, 7), is(11));
+    assertThat(BasicOperations.basicMath("-", 15, 18), is(-3));
+    assertThat(BasicOperations.basicMath("*", 5, 5), is(25));
+    assertThat(BasicOperations.basicMath("/", 49, 7), is(7));
+  }
+}
