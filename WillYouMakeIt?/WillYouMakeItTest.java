51a2f6709b5d2bdeadf36ab03bdd128fa6fffc43
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-12 10:52:17 +0200

    Update WillYouMakeItTest.java

--- WillYouMakeIt?/WillYouMakeItTest.java
+++ WillYouMakeIt?/WillYouMakeItTest.java
@@ -1,21 +1,12 @@
-public class Kata {
-  
-  public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {  
-    if(mpg*fuelLeft>=distanceToPump) {
-    return true;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void testSomething() {
+        assertTrue(Kata.zeroFuel(50, 25, 2));
+        assertFalse(Kata.zeroFuel(100, 50, 1));
     }
-    else {
-    return false;
-    } 
-  } 
 }
-
-
-// DESCRIPTION:
-// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
-
-// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
-
-// Function should return true if it is possible and false if not.
-
-// MATHEMATICS FUNDAMENTALS
