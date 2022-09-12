9723a8d4a67218c97d974ff4e1d86cff7c2f7eb4
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-12 10:59:19 +0200

    Create BeginnerSeries#4CockroachTest.java

---
+++ BeginnerSeries#4Cockroach/BeginnerSeries#4CockroachTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CockroachSpeedTests{
+  @Test
+  public void basicTests() throws Exception{
+    Cockroach cockroach = new Cockroach();
+    assertEquals(30, cockroach.cockroachSpeed(1.08));
+    assertEquals(30, cockroach.cockroachSpeed(1.09));
+    assertEquals(0, cockroach.cockroachSpeed(0));
+  }
+}
