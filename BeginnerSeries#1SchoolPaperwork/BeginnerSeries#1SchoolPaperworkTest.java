6674088b9cfe9df1c60552770a05a3ee2c9685f9
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-12 10:57:35 +0200

    Create BeginnerSeries#1SchoolPaperworkTest.java

---
+++ BeginnerSeries#1SchoolPaperwork/BeginnerSeries#1SchoolPaperworkTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class PaperTests {
+
+    @Test
+    public void test1(){
+         assertEquals("Failed at paperWork(5,5)",25, Paper.paperWork(5,5));
+    }
+  
+    @Test
+    public void test2(){
+        assertEquals("Failed at paperWork(5,-5)",0, Paper.paperWork(5,-5));
+    }
+    
+    @Test
+    public void test3(){
+        assertEquals("Failed at paperWork(-5,-5)",0, Paper.paperWork(-5,-5));
+    }
+    
+    @Test
+    public void test4(){
+        assertEquals("Failed at paperWork(-5,5)",0, Paper.paperWork(-5,5));
+    }
+    
+    @Test
+    public void test5(){
+        assertEquals("Failed at paperWork(5,0)",0, Paper.paperWork(5,0));
+    }
+}
