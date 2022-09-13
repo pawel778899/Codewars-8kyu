c88033f88070b232b6b4a94436eece39c1806c51
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 08:42:56 +0200

    Create IsNDivisibleByXAndY?Test.java

---
+++ IsNDivisibleByXAndY?/IsNDivisibleByXAndY?Test.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class DivisibleNbTests {
+
+	@Test
+	public void test1() {
+		assertEquals(true, DivisibleNb.isDivisible(12,4,3));
+	}
+	@Test
+	public void test2() {
+		assertEquals(false, DivisibleNb.isDivisible(3,3,4));
+	}	
+		
+}
