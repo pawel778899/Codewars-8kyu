72897624f5bbf72dabf252b4d9f19e5725a8fac3
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 07:57:11 +0200

    Create EasyLogs.java

---
+++ EasyLogs/EasyLogs.java
@@ -0,0 +1,16 @@
+public class EasyLogs {
+
+  public static double logs(double x, double a, double b) {
+    return (Math.log(a) + Math.log(b)) / Math.log(x);
+  }
+
+}
+
+
+
+
+
+// DESCRIPTION:
+// Add two logs with base X, with the value of A and B. Example log A + log B where the base is X.
+
+// ALGORITHMS
