1d9c2f4bfd133c9258fc973988e0d551e8fcd43e
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 09:01:30 +0200

    Create PushAHash-AnObjectIntoArray.java

---
+++ PushAHash-AnObjectIntoArray/PushAHash-AnObjectIntoArray.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class PushAnObjectIntoArray {  
+  public static List<String> push(){
+    List<String> items = new ArrayList<>();
+    items.add("an object");
+    return items;
+  }
+}
+
+
+// DESCRIPTION:
+// You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+
+// ARRAYS DEBUGGING
