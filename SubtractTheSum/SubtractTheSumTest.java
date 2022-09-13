a67dca95d4a1a6f296fe82bec0017f52a74a9044
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 09:39:57 +0200

    Create SubtractTheSumTest.java

---
+++ SubtractTheSum/SubtractTheSumTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void basicTest() {
+         assertEquals("apple", Kata.subtractSum(10));
+    }
+}
