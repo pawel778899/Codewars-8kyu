a07c71b8fb2c6703fa21ecdd68443b9fcc81e31b
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 07:37:44 +0200

    Create AWolfInSheep'sClothing.java

---
+++ AWolfInSheep'sClothing/AWolfInSheep'sClothing.java
@@ -0,0 +1,48 @@
+//Solution 1
+
+import java.util.*;
+
+public class ZywOo {
+
+  public static String warnTheSheep(String[] array) {
+    Collections.reverse(Arrays.asList(array));
+    int pos = Arrays.asList(array).indexOf("wolf");
+    return pos == 0 ? "Pls go away and stop eating my sheep" : "Oi! Sheep number " + pos + "! You are about to be eaten by a wolf!";
+  }
+
+}
+
+
+//Solution 2
+
+import static java.util.List.of;
+
+interface ZywOo {
+  static String warnTheSheep(String[] array) {
+    int wolfN = of(array).indexOf("wolf") + 1;
+    return wolfN == array.length ? "Pls go away and stop eating my sheep" : "Oi! Sheep number " + (array.length - wolfN) + "! You are about to be eaten by a wolf!";
+  }
+}
+
+
+
+
+DESCRIPTION:
+Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. Fortunately, you are good at spotting them.
+
+Warn the sheep in front of the wolf that it is about to be eaten. Remember that you are standing at the front of the queue which is at the end of the array:
+
+[sheep, sheep, sheep, sheep, sheep, wolf, sheep, sheep]      (YOU ARE HERE AT THE FRONT OF THE QUEUE)
+   7      6      5      4      3            2      1
+If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+Note: there will always be exactly one wolf in the array.
+
+Examples
+Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+Input: ["sheep", "sheep", "wolf"]
+Output: "Pls go away and stop eating my sheep"
+
+FUNDAMENTALS ARRAYS
