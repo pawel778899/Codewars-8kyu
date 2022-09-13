a45a5021ac519b0659195e5ef5b51b1299cc66b1
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 07:41:37 +0200

    Create AreaOfASquare.java

---
+++ AreaOfASquare/AreaOfASquare.java
@@ -0,0 +1,17 @@
+class Geometry {
+  static double squareArea(double A) {
+    return Math.round(Math.pow(.636619 * A, 2) * 100) / 100.;
+  }
+}
+
+
+
+
+// DESCRIPTION:
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+// Graph
+
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+// PUZZLES
