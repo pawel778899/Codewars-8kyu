a15c48cdec10d822dfc59970341bc31215b82197
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 09:01:47 +0200

    Create  PushAHash-AnObjectIntoArrayTest.java

---
+++ PushAHash-AnObjectIntoArray/PushAHash-AnObjectIntoArrayTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.util.List;
+
+public class PushAnObjectIntoArrayTest {
+
+    @Test
+    public void testSomething() {
+      List<String> items = PushAnObjectIntoArray.push();
+      assertEquals(1,items.size());
+      assertEquals("an object", items.get(0));
+    }
+}
