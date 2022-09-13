e3ead2a301f6fb034f65ba8601850cf13e882cb8
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 08:27:55 +0200

    Create GhostCode?!.java

---
+++ GhostCode?!/GhostCode?!.java
@@ -0,0 +1,34 @@
+//solution 1
+
+public class GhostCode{
+  public static String helloName(String name) {
+    return "Hello " + (name == null || name.isEmpty() ? "world!" : "my name is " + name);
+  }
+}
+
+//Solution 2
+
+public class GhostCode{
+  public static String helloName(final String name){
+    if(name == null || name.equals(""))
+      return "Hello world!";
+    else
+      return "Hello my name is " + name;
+  }
+}
+
+
+DESCRIPTION:
+Trollencio changed my code! can you fix it? My name is not "dumb!" o( TT_TT )o
+
+Instructions: Fix the code
+
+Input
+Javatlacati
+Output
+Hello my name is Javatlacati
+Input
+""
+Output
+Hello world!
+UNICODE DEBUGGING
