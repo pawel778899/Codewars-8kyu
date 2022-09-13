19384f8f4d1f8ff4ea35a6e86353b9748bbf8a45
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 09:17:14 +0200

    Create WilsonPrimes.java

---
+++ WilsonPrimes/WilsonPrimes.java
@@ -0,0 +1,21 @@
+public class WilsonPrime {
+	public static boolean am_i_wilson(double n) {
+    return n == 5 || n == 13 || n == 563;
+  }
+}
+
+
+
+
+
+// DESCRIPTION:
+// Wilson primes satisfy the following condition. Let P represent a prime number.
+
+// Then,
+
+// ((P-1)! + 1) / (P * P)
+// should give a whole number.
+
+// Your task is to create a function that returns true if the given number is a Wilson prime.
+
+// MATHEMATICS FUNDAMENTALS
