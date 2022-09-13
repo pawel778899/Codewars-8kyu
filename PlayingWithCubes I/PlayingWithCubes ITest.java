038fd5c84a3202a298bfad62950dc282cc6d4ce6
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 08:57:02 +0200

    Create PlayingWithCubes ITest.java

---
+++ PlayingWithCubes I/PlayingWithCubes ITest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+
+public class SolutionTest {
+    @Test
+    public void testSetToThree() {
+        Cube c = new Cube();
+        c.setSide(3);
+        assertEquals(3, c.getSide());
+    }
+}
