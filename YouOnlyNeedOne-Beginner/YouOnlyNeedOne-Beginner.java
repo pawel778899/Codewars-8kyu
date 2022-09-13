25ada8741d5127071457e027ee998383453770e6
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 07:30:09 +0200

    Update YouOnlyNeedOne-Beginner.java

--- YouOnlyNeedOne-Beginner/YouOnlyNeedOne-Beginner.java
+++ YouOnlyNeedOne-Beginner/YouOnlyNeedOne-Beginner.java
@@ -5,3 +5,16 @@ interface Solution {
     return of(a).contains(x);
   }
 }
+
+
+
+
+
+// DESCRIPTION:
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// FUNDAMENTALS STRINGS ARRAYS
