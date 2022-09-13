068033cbfcc3d443df1099760d1e7f4cd2cb8a83
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-13 09:47:04 +0200

    Create StringTemplates-BugFixing#5.java

---
+++ StringTemplates-BugFixing#5/StringTemplates-BugFixing#5.java
@@ -0,0 +1,13 @@
+public class Kata {
+  public static String buildString(String... args) {
+    return "I like " + String.join(", ", args) + "!";
+  }
+}
+
+
+
+// DESCRIPTION:
+// Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+// ◄PREVIOUS KATANEXT KATA►
+// STRINGS DEBUGGING
