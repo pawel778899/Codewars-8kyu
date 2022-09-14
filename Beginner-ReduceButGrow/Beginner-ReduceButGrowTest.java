ec1926279df9c1dff94d9ddb54d9dac287d58dc9
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 07:16:41 +0200

    Create Beginner-ReduceButGrowTest.java

---
+++ Beginner-ReduceButGrow/Beginner-ReduceButGrowTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+import java.util.*;
+
+public class SolutionTest {
+    @Test
+    public void testSomething() {
+         assertEquals(6, Kata.grow(new int[]{1,2,3}));
+         assertEquals(16, Kata.grow(new int[]{4,1,1,1,4}));
+         assertEquals(64, Kata.grow(new int[]{2,2,2,2,2,2}));
+    }
+}
