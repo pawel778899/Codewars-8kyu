8cd7b1e3c4b57f2a84e0223fca47876227f6d52e
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:10:58 +0200

    Create IsItANumber?Test.java

---
+++ IsItANumber?/IsItANumber?Test.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DigitTest {
+  
+    MyUtilities myUtil = new MyUtilities();
+
+@Test
+    public void test0() throws Exception 
+    {   
+        assertEquals(false,myUtil.isDigit("s2324"));
+    }
+     @Test
+    public void test1() throws Exception 
+    {   
+        assertEquals(true,myUtil.isDigit("-234.4"));
+    }
+
+}
