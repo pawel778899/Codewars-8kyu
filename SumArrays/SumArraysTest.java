3cb7cb1152c9c9c665fe173802ba009931f50e10
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:43:37 +0200

    Create SumArraysTest.java

---
+++ SumArrays/SumArraysTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SumArrayTest {
+
+  @Test
+  public void testSum() {
+    assertEquals(0,      SumArray.sum(new double[] { }),                1e-4);
+    assertEquals(-2.398, SumArray.sum(new double[] {-2.398}),           1e-4);
+    assertEquals(6,      SumArray.sum(new double[] {1, 2, 3}),          1e-4);
+    assertEquals(6.6,    SumArray.sum(new double[] {1.1, 2.2, 3.3}),    1e-4);
+    assertEquals(9.2,    SumArray.sum(new double[] {1, 5.2, 4, 0, -1}), 1e-4);
+    assertEquals(320,    SumArray.sum(new double[] {30, 89, 100, 101}), 1e-4);
+  }
+}
