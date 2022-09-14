a53bf9207ebdd262de1366267c4262bc4eab603e
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 07:23:50 +0200

    Create CompareWithinMarginTest.java

---
+++ CompareWithinMarginTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    @Test
+    public void exampleTestCases() {
+        assertEquals(-1, Solution.closeCompare(4, 5));
+        assertEquals(0, Solution.closeCompare(5, 5));
+        assertEquals(1, Solution.closeCompare(6, 5));
+        assertEquals(-1, Solution.closeCompare(-6, -5));
+
+        assertEquals(0, Solution.closeCompare(2, 5, 3));
+        assertEquals(1, Solution.closeCompare(8.1, 5, 3));
+        assertEquals(-1, Solution.closeCompare(1.99, 5, 3));
+    }
+
+}
