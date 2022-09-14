14c105cc6b5d1a87b66af603c7b670eee8a0844c
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:02:14 +0200

    Create Grasshopper-PersonalizedMessageTest.java

---
+++ Grasshopper-PersonalizedMessage/Grasshopper-PersonalizedMessageTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class KataTest {
+    @Test public void basicTests() {
+        assertEquals("For inputs \"Daniel\" and \"Daniel\"", "Hello boss", Kata.greet("Daniel", "Daniel"));
+        assertEquals("For inputs \"Greg\" and \"Daniel\"", "Hello guest", Kata.greet("Greg", "Daniel"));
+    }
+}
