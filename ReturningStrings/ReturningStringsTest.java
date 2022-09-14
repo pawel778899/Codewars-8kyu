b96bc701d88ce6d2b5b943b49256f1211e0ed97b
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:32:58 +0200

    Create ReturningStringsTest.java

---
+++ ReturningStrings/ReturningStringsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void testSomething() {
+        assertEquals("Hello, Ryan how are you doing today?", Kata.greet("Ryan"));
+    }
+}
