851c6c911aa3ac6719e238a80e0fc1baedaa4e59
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:34:17 +0200

    Create ReversedSequenceTest.java

---
+++ ReversedSequence/ReversedSequenceTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.runners.JUnit4;
+
+
+public class SolutionTest {
+     @Test
+    public void simpleTest() {
+        assertArrayEquals(new int[]{5,4,3,2,1},Sequence.reverse(5));
+    }
+}
