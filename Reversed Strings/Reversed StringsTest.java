9f2443e7e4306d09101cae7c6a320068aa908e37
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:33:40 +0200

    Create Reversed StringsTest.java

---
+++ Reversed Strings/Reversed StringsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void sampleTests() {
+      assertEquals("dlrow", Kata.solution("world"));
+    }
+}
