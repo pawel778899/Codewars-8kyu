fcc7bbbff5c347beecbd5beec80ddf172d942a85
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:23:17 +0200

    Create QuarterOfTheYearTest.java

---
+++ QuarterOfTheYear/QuarterOfTheYearTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class SampleTest {
+    @Test
+    public void exampleTests() {
+        Tester.doTest( 3, 1);
+        Tester.doTest( 8, 3);
+        Tester.doTest(11, 4);
+    }
+}
