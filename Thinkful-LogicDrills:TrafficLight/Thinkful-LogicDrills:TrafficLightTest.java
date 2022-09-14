1521e096073d6a3c261b75a39a60b717e7f517f9
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 10:00:11 +0200

    Create Thinkful-LogicDrills:TrafficLightTest.java

---
+++ Thinkful-LogicDrills:TrafficLight/Thinkful-LogicDrills:TrafficLightTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void update_light() {
+        assertEquals("green", TrafficLights.updateLight("red"));
+        assertEquals("yellow", TrafficLights.updateLight("green"));
+        assertEquals("red", TrafficLights.updateLight("yellow"));
+    }
+}
