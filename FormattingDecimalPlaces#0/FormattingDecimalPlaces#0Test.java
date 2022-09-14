f2c180386ca4d95aaaf519636c36a5bf2995dd05
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 08:53:37 +0200

    Create FormattingDecimalPlaces#0Test.java

---
+++ FormattingDecimalPlaces#0/FormattingDecimalPlaces#0Test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class NumbersTest
+{
+  @Test
+  public void Test_01()
+  {
+    assertEquals(4.66, Numbers.TwoDecimalPlaces(4.659725356), 0.00);
+  }
+  
+  @Test
+  public void Test_02()
+  {
+    assertEquals(173735326.38, Numbers.TwoDecimalPlaces(173735326.3783732637948948), 0.00);
+  }
+}
