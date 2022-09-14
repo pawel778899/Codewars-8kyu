789eb0fa2e2488f49b77d58e200dca702266edfa
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:44:05 +0200

    Update SumMixedArray.java

--- SumMixedArray/SumMixedArray.java
+++ SumMixedArray/SumMixedArray.java
@@ -9,10 +9,10 @@ public class MixedSum {
 
 
 
-DESCRIPTION:
-Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// DESCRIPTION:
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
 
-Return your answer as a number.
+// Return your answer as a number.
 
-FUNDAMENTALS STRINGS ARRAYS
+// FUNDAMENTALS STRINGS ARRAYS
 
