79c086b1395214c459cc3869fbac6b8bf5d1f43d
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:59:32 +0200

    Update and rename Thinkful-LogicDrills:TrafficLight.java to Thinkful-LogicDrills:TrafficLight/Thinkful-LogicDrills:TrafficLight.java

--- Thinkful-LogicDrills:TrafficLight/Thinkful-LogicDrills:TrafficLight.java
+++ Thinkful-LogicDrills:TrafficLight/Thinkful-LogicDrills:TrafficLight.java
@@ -30,11 +30,11 @@ public class TrafficLights {
 
 
 
-DESCRIPTION:
-You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// DESCRIPTION:
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
 
-Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
 
-For example, when the input is green, output should be yellow.
+// For example, when the input is green, output should be yellow.
 
-FUNDAMENTALS
+// FUNDAMENTALS
