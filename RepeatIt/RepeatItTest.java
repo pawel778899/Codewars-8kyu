371b0ce79c7bd831d342c50a7060b9735c33144c
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:26:51 +0200

    Create RepeatItTest.java

---
+++ RepeatIt/RepeatItTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RepeatItTest {
+  
+  @Test
+  public void testStar() {
+    repeatTest("*", 3, "***");
+  }
+  
+  @Test
+  public void testHello() {
+    repeatTest("Hello", 11, "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello");
+  }
+  
+  public void repeatTest(final Object toRepeat, final int n, final String expected) {
+    final String testMessage = String.format("Testing with toRepeat = %s and n = %d", toRepeat, n);
+    assertEquals(testMessage,  expected, RepeatIt.repeatString(toRepeat, n));
+  }
+}
