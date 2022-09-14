5ed78236b8f4aa7f5af4b42ad19ec97907e7ced9
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 08:41:55 +0200

    Create EnumerableMagic#25-TakeTheFirstNElementsTest.java

---
+++ EnumerableMagic#25-TakeTheFirstNElements/EnumerableMagic#25-TakeTheFirstNElementsTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void basicTests() {
+        assertArrayEquals("should return the first 3 items", new int[]{0, 1, 2}, ZywOo.take(new int[]{0, 1, 2, 3, 5, 8, 13}, 3));
+    }
+}
