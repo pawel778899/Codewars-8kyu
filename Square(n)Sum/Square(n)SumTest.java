88be91177e7f3e3f15333739a5c2898f939a84fc
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:40:48 +0200

    Create Square(n)SumTest.java

---
+++ Square(n)Sum/Square(n)SumTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Tests
+{
+  @Test
+  public void testBasic()
+  {
+   assertEquals(9, Kata.squareSum(new int[] {1,2,2}));
+   assertEquals(5, Kata.squareSum(new int[] {1,2}));
+   assertEquals(50, Kata.squareSum(new int[] {5,-3,4}));
+   assertEquals(0, Kata.squareSum(new int[] {}));
+  }
+}
