47de32ead30bcb9832251f3a2acaf46261083109
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 07:15:32 +0200

    Create BasicVariableAssignmentTest.java

---
+++ BasicVariableAssignment/BasicVariableAssignmentTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class Tests {
+    @Test
+    public void correctValue() {
+        assertEquals("codewa.rs", Kata.name);
+    }
+}
