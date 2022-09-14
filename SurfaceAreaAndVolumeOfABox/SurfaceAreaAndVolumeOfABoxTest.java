c4ce7347b989fcd96b26a7b4f81b40f5233d1021
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:49:31 +0200

    Create SurfaceAreaAndVolumeOfABoxTest.java

---
+++ SurfaceAreaAndVolumeOfABox/SurfaceAreaAndVolumeOfABoxTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void test1() {
+        Kata k = new Kata();
+        
+        assertArrayEquals(new int[] { 88, 48 }, k.getSize(4, 2, 6));
+        assertArrayEquals(new int[] { 6, 1 }, k.getSize(1, 1, 1));
+        assertArrayEquals(new int[] { 10, 2 }, k.getSize(1, 2, 1));
+        assertArrayEquals(new int[] { 16, 4 }, k.getSize(1, 2, 2));
+        assertArrayEquals(new int[] { 600, 1000 }, k.getSize(10, 10, 10));
+    }
+}
