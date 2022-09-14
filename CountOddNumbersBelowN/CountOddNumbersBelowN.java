7718bccf0da84dc98acabab113055182ea609fad
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 08:32:03 +0200

    Create CountOddNumbersBelowN.java

---
+++ CountOddNumbersBelowN/CountOddNumbersBelowN.java
@@ -0,0 +1,18 @@
+public class Kata {
+  public static int oddCount(int n){
+    return n/2;  
+  }
+}
+
+
+
+
+
+
+// DESCRIPTION:
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// Examples (Input -> Output)
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+// Expect large Inputs!
