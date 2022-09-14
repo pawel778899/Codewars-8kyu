ce5381fd5165cfeb7665371cfde94df37839870b
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-14 09:24:00 +0200

    Create RegexpBasics-IsItADigit?Test.java

---
+++ RegexpBasics-IsItADigit?/RegexpBasics-IsItADigit?Test.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class StringUtilsTest {
+  
+  @Test
+  public void fixedTests() {
+    assertFalse(StringUtils.isDigit(""));
+    assertTrue(StringUtils.isDigit("7"));
+    assertFalse(StringUtils.isDigit(" "));
+    assertFalse(StringUtils.isDigit("a"));
+    assertFalse(StringUtils.isDigit("a5"));
+    assertFalse(StringUtils.isDigit("14"));
+  }
+}
