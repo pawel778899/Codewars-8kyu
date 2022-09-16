b0418462defa9884e5fc0e23204ebee4ec553207
Paweł <106928352+pawel778899@users.noreply.github.com>
2022-09-16 13:01:52 +0200

    Update ArrayPlusArrayTest.java

--- ArrayPlusArray/ArrayPlusArrayTest.java
+++ ArrayPlusArray/ArrayPlusArrayTest.java
@@ -2,8 +2,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
 
-// TODO: Replace examples and use TDD development by writing your own tests
-
 public class SolutionTest {
     @Test
     public void sampleTests() {
